c49d1e3d7be4d22b900d4e0e81122a713191a5f6
higebobo <higebobo@gmail.com>
2020-10-19 15:52:13 +0900

    step 1

--- app/main.py
+++ app/main.py
@@ -6,4 +6,6 @@ app = FastAPI()
 
 @app.get("/ping")
 def pong():
+    # some async operation could happen here
+    # example: `notes = await get_all_notes()`
     return {"ping": "pong!"}
---
+++ tests/test_main.py
@@ -0,0 +1,5 @@
+# -*- mode: python -*- -*- coding: utf-8 -*-
+def test_ping(test_app):
+    response = test_app.get("/ping")
+    assert response.status_code == 200
+    assert response.json() == {"ping": "pong!"}
